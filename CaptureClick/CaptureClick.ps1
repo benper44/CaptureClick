f529efc3918473403b79d7835568c533b63b1c92
Benjamin <ben.perrier@outlook.fr>
2019-02-21 09:38:20 +0100

    First commit CaptureClick

---
+++ CaptureClick/CaptureClick.ps1
@@ -0,0 +1,311 @@
+#
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+    <#
+        .SYNOPSIS
+        CaptureClick makes Screenshot of your first screen evertime you Left click.
+
+        .DESCRIPTION
+        CaptureClick is a tool to make screenshots of your first screen everytime you Left click with your mouse or touchpad.
+        
+        All captures are save in folder automaticly created by CaptureClick. 
+        The folder is created where you start the script/program and has the following name CaptureClick_DATE-TIME.
+        
+        To stop CaptureClick you have to hold your right mouse button until a popup windows says the script/program is done. 
+
+        .NOTES
+        Version:    1.0
+        Author:     <Benjamin PERRIER>
+        Creation Date:  <16/02/2019>
+        Script Name: CaptureClick
+
+    #>
+
+Function Take-ScreenShot { 
+   
+        [cmdletbinding( 
+                SupportsShouldProcess = $True, 
+                DefaultParameterSetName = "screen", 
+                ConfirmImpact = "low" 
+        )] 
+Param ( 
+       [Parameter( 
+            Mandatory = $False, 
+            ParameterSetName = "screen", 
+            ValueFromPipeline = $True)] 
+            [switch]$screen, 
+       [Parameter( 
+            Mandatory = $False, 
+            ParameterSetName = "window", 
+            ValueFromPipeline = $False)] 
+            [switch]$activewindow, 
+       [Parameter( 
+            Mandatory = $False, 
+            ParameterSetName = "", 
+            ValueFromPipeline = $False)] 
+            [string]$file,  
+       [Parameter( 
+            Mandatory = $False, 
+            ParameterSetName = "", 
+            ValueFromPipeline = $False)] 
+            [string] 
+            [ValidateSet("bmp","jpeg","png")] 
+            $imagetype = "bmp", 
+       [Parameter( 
+            Mandatory = $False, 
+            ParameterSetName = "", 
+            ValueFromPipeline = $False)] 
+            [switch]$print                        
+        
+) 
+# C# code 
+$code = @' 
+using System; 
+using System.Runtime.InteropServices; 
+using System.Drawing; 
+using System.Drawing.Imaging; 
+namespace ScreenShotDemo 
+{ 
+  /// <summary> 
+  /// Provides functions to capture the entire screen, or a particular window, and save it to a file. 
+  /// </summary> 
+  public class ScreenCapture 
+  { 
+    /// <summary> 
+    /// Creates an Image object containing a screen shot the active window 
+    /// </summary> 
+    /// <returns></returns> 
+    public Image CaptureActiveWindow() 
+    { 
+      return CaptureWindow( User32.GetForegroundWindow() ); 
+    } 
+    /// <summary> 
+    /// Creates an Image object containing a screen shot of the entire desktop 
+    /// </summary> 
+    /// <returns></returns> 
+    public Image CaptureScreen() 
+    { 
+      return CaptureWindow( User32.GetDesktopWindow() ); 
+    }     
+    /// <summary> 
+    /// Creates an Image object containing a screen shot of a specific window 
+    /// </summary> 
+    /// <param name="handle">The handle to the window. (In windows forms, this is obtained by the Handle property)</param> 
+    /// <returns></returns> 
+    private Image CaptureWindow(IntPtr handle) 
+    { 
+      // get te hDC of the target window 
+      IntPtr hdcSrc = User32.GetWindowDC(handle); 
+      // get the size 
+      User32.RECT windowRect = new User32.RECT(); 
+      User32.GetWindowRect(handle,ref windowRect); 
+      int width = windowRect.right - windowRect.left; 
+      int height = windowRect.bottom - windowRect.top; 
+      // create a device context we can copy to 
+      IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc); 
+      // create a bitmap we can copy it to, 
+      // using GetDeviceCaps to get the width/height 
+      IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc,width,height); 
+      // select the bitmap object 
+      IntPtr hOld = GDI32.SelectObject(hdcDest,hBitmap); 
+      // bitblt over 
+      GDI32.BitBlt(hdcDest,0,0,width,height,hdcSrc,0,0,GDI32.SRCCOPY); 
+      // restore selection 
+      GDI32.SelectObject(hdcDest,hOld); 
+      // clean up 
+      GDI32.DeleteDC(hdcDest); 
+      User32.ReleaseDC(handle,hdcSrc); 
+      // get a .NET image object for it 
+      Image img = Image.FromHbitmap(hBitmap); 
+      // free up the Bitmap object 
+      GDI32.DeleteObject(hBitmap); 
+      return img; 
+    } 
+    /// <summary> 
+    /// Captures a screen shot of the active window, and saves it to a file 
+    /// </summary> 
+    /// <param name="filename"></param> 
+    /// <param name="format"></param> 
+    public void CaptureActiveWindowToFile(string filename, ImageFormat format) 
+    { 
+      Image img = CaptureActiveWindow(); 
+      img.Save(filename,format); 
+    } 
+    /// <summary> 
+    /// Captures a screen shot of the entire desktop, and saves it to a file 
+    /// </summary> 
+    /// <param name="filename"></param> 
+    /// <param name="format"></param> 
+    public void CaptureScreenToFile(string filename, ImageFormat format) 
+    { 
+      Image img = CaptureScreen(); 
+      img.Save(filename,format); 
+    }     
+    
+    /// <summary> 
+    /// Helper class containing Gdi32 API functions 
+    /// </summary> 
+    private class GDI32 
+    { 
+       
+      public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter 
+      [DllImport("gdi32.dll")] 
+      public static extern bool BitBlt(IntPtr hObject,int nXDest,int nYDest, 
+        int nWidth,int nHeight,IntPtr hObjectSource, 
+        int nXSrc,int nYSrc,int dwRop); 
+      [DllImport("gdi32.dll")] 
+      public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC,int nWidth, 
+        int nHeight); 
+      [DllImport("gdi32.dll")] 
+      public static extern IntPtr CreateCompatibleDC(IntPtr hDC); 
+      [DllImport("gdi32.dll")] 
+      public static extern bool DeleteDC(IntPtr hDC); 
+      [DllImport("gdi32.dll")] 
+      public static extern bool DeleteObject(IntPtr hObject); 
+      [DllImport("gdi32.dll")] 
+      public static extern IntPtr SelectObject(IntPtr hDC,IntPtr hObject); 
+    } 
+ 
+    /// <summary> 
+    /// Helper class containing User32 API functions 
+    /// </summary> 
+    private class User32 
+    { 
+      [StructLayout(LayoutKind.Sequential)] 
+      public struct RECT 
+      { 
+        public int left; 
+        public int top; 
+        public int right; 
+        public int bottom; 
+      } 
+      [DllImport("user32.dll")] 
+      public static extern IntPtr GetDesktopWindow(); 
+      [DllImport("user32.dll")] 
+      public static extern IntPtr GetWindowDC(IntPtr hWnd); 
+      [DllImport("user32.dll")] 
+      public static extern IntPtr ReleaseDC(IntPtr hWnd,IntPtr hDC); 
+      [DllImport("user32.dll")] 
+      public static extern IntPtr GetWindowRect(IntPtr hWnd,ref RECT rect); 
+      [DllImport("user32.dll")] 
+      public static extern IntPtr GetForegroundWindow();       
+    } 
+  } 
+} 
+'@ 
+#User Add-Type to import the code 
+add-type $code -ReferencedAssemblies 'System.Windows.Forms','System.Drawing' 
+#Create the object for the Function 
+$capture = New-Object ScreenShotDemo.ScreenCapture 
+ 
+#Take screenshot of the entire screen 
+If ($Screen) { 
+    Write-Verbose "Taking screenshot of entire desktop" 
+    #Save to a file 
+    If ($file) { 
+        If ($file -eq "") { 
+            $file = "$pwd\image.bmp" 
+            } 
+        Write-Verbose "Creating screen file: $file with imagetype of $imagetype" 
+        $capture.CaptureScreenToFile($file,$imagetype) 
+        } 
+    ElseIf ($print) { 
+        $img = $Capture.CaptureScreen() 
+        $pd = New-Object System.Drawing.Printing.PrintDocument 
+        $pd.Add_PrintPage({$_.Graphics.DrawImage(([System.Drawing.Image]$img), 0, 0)}) 
+        $pd.Print() 
+        }         
+    Else { 
+        $capture.CaptureScreen() 
+        } 
+    } 
+#Take screenshot of the active window     
+If ($ActiveWindow) { 
+    Write-Verbose "Taking screenshot of the active window" 
+    #Save to a file 
+    If ($file) { 
+        If ($file -eq "") { 
+            $file = "$pwd\image.bmp" 
+            } 
+        Write-Verbose "Creating activewindow file: $file with imagetype of $imagetype" 
+        $capture.CaptureActiveWindowToFile($file,$imagetype) 
+        } 
+    ElseIf ($print) { 
+        $img = $Capture.CaptureActiveWindow() 
+        $pd = New-Object System.Drawing.Printing.PrintDocument 
+        $pd.Add_PrintPage({$_.Graphics.DrawImage(([System.Drawing.Image]$img), 0, 0)}) 
+        $pd.Print() 
+        }         
+    Else { 
+        $capture.CaptureActiveWindow() 
+        }     
+    }      
+} 
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")    
+
+$CurrentProcess = [System.Diagnostics.Process]::GetCurrentProcess() | Select-Object -ExpandProperty Id
+$ParentProcess = gwmi Win32_Process -Filter "processid = '$CurrentProcess'" | Select-Object -ExpandProperty ParentProcessId
+$ProcessPath = Get-Process -Id $ParentProcess  | Select-Object -ExpandProperty Path | Split-Path
+
+if ($ProcessPath -match "Windows"){
+$ProcessPath = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+}
+
+$name = Get-Date -UFormat "CaptureClick_%d.%m.%Y-%H.%M.%S"
+$path ="$ProcessPath\"
+$pathname = "$path$name"
+New-Item -ItemType directory -Path $pathname
+
+$nb=1
+$rightquit = 0
+$count = 0
+
+Clear-Host
+$msg = "CaptureClick will capture your screen every Left click in : 
+
+$pathname
+
+To kill CaptureClick hold Right click until a message appears."
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+$ans = [System.Windows.Forms.MessageBox]::Show($msg , "CaptureClick" , "Ok", "Question", "Button1", "DefaultDesktopOnly")
+        
+
+
+Clear-Host
+While ($True) {
+
+if ($count -eq 10) {
+$rightquit = 0
+$count = 0
+}
+
+$mouse = [System.Windows.Forms.UserControl]::MouseButtons
+
+
+if ($mouse -eq "Left"){
+$name = "CaptureClick_$nb.png"
+$path = $pathname
+$pathnamefile = "$path\$name"
+Take-ScreenShot -screen -file $pathnamefile -imagetype png
+$pathnamefile 
+$nb++
+}
+
+if ($mouse -eq "Right"){$rightquit++}
+if ($rightquit -gt 9){break}
+
+Start-Sleep -m 150
+$count++
+
+}
+
+$msg = "End of CaptureClick!"
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+$ans = [System.Windows.Forms.MessageBox]::Show($msg , "CaptureClick" , "Ok", "Question", "Button1", "DefaultDesktopOnly")
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # CaptureClick
+
+CaptureClick makes Screenshot of your first screen evertime you Left click.
+
+# Usage
+
+CaptureClick is a tool to make screenshots of your first screen everytime you Left click with your mouse or touchpad.
+        
+All captures are save in folder automaticly created by CaptureClick. 
+The folder is created where you start the script/program and has the following name CaptureClick_DATE-TIME.
+        
+To stop CaptureClick you have to hold your right mouse button until a popup windows says the script/program is done. 
+
+# Requirements
+
+- Has only be tested on Windows 10 x64
+
+# Issue
+
+
+# Author
+
+**Benjamin PERRIER**
+
+- <https://github.com/benper44>
+
+# Special Thanks
+
+- Gallery Technet Microsoft for Take-Screebshot function (https://gallery.technet.microsoft.com/scriptcenter/eeff544a-f690-4f6b-a586-11eea6fc5eb8)
+
+# License
+
+Copyright 2019 Benjamin PERRIER.
